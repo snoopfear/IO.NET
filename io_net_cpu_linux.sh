d1d0cdf293843d36593ca945279066c965d397b7
snoopfear <39308472+snoopfear@users.noreply.github.com>
2024-03-11 01:11:52 +0300

    Update io_net_cpu_linux.sh

--- io_net_cpu_linux.sh
+++ io_net_cpu_linux.sh
@@ -28,8 +28,7 @@ sudo apt install -y curl
 
 # Загружаем и запускаем скрипт установки ionet
 curl -L https://github.com/ionet-official/io-net-official-setup-script/raw/main/ionet-setup.sh -o ionet-setup.sh
-chmod +x ionet-setup.sh
-./ionet-setup.sh
+chmod +x ionet-setup.sh && ./ionet-setup.sh
 
 # Загружаем и устанавливаем исполняемый файл launch_binary_linux
 curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/launch_binary_linux -o launch_binary_linux
@@ -37,4 +36,3 @@ chmod +x launch_binary_linux
 
 # Запускаем launch_binary_linux с нужными параметрами, подставив значение переменной $NODENAME
 ./launch_binary_linux --device_id=044b39b8-6bc8-42f0-ad7d-5f33c504f549 --user_id=820cb56b-3d52-4099-a359-bfd8de33c5ca --operating_system="Linux" --usegpus=false --device_name=$NODENAME
-docker ps
